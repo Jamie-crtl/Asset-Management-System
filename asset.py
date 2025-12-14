a320b7a3fa8677462a623cd0cf07d36819d3e026
ss1354 <ss1354@student.le.ac.uk>
2025-12-14 17:00:42 +0000

    fixed allowed categories and statuses

--- asset.py
+++ asset.py
@@ -1,10 +1,11 @@
 class Asset:
+    ALLOWED_CATEGORIES = {"property", "vehicle", "other"}
+    ALLOWED_STATUSES = {"available", "assigned", "disposed"}
     def __init__(self, id, name, category, value, status, assigned_to = None, history = None):
-        ALLOWED_CATEGORIES = {"property","vehicle","other"}
-        ALLOWED_STATUSES = {"available", "assigned","disposed"}
-        if category not in ALLOWED_CATEGORIES:
+
+        if category not in Asset.ALLOWED_CATEGORIES:
             raise ValueError("Category must be one of the following values: property, vehicle, other")
-        if status not in ALLOWED_STATUSES:
+        if status not in Asset.ALLOWED_STATUSES:
             raise ValueError("Status must be one of the following values: available, assigned, disposed")
         self.id = id
         self.name = name
