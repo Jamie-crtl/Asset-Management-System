44a974d1faa4086b595675dd65f40328d513bfe5
sf361 <sf361@student.le.ac.uk>
2025-12-27 20:36:37 +0000

    updating main.py validation

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from asset_manager import AssetManager
 def main():
     manager = AssetManager()
     print("Asset manager system")
-    manager.create_new_asset({"asset_id": 1, "name": "Asset 1","category": "Asset","value": 500,"status": "Active"}) #used to create an asset
+    manager.create_new_asset({
+        "asset_id": "1",
+        "name": "Asset 1",
+        "category": "property",
+        "value": 500,
+        "status": "available",
+        "assigned_to": None,
+        "history": []
+    }) #used to create an asset
 
 if __name__ == "__main__":
     main()
